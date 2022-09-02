b5533fc35483b70f3f84bf36361d8dd1c67a4355
Pratik Das <pratik14901@gmail.com>
2022-09-03 00:58:58 +0530

    Assignemt 1 a

---
+++ JavaScript/Assignment 1/Asgn1.js
@@ -0,0 +1,13 @@
+//Assignment 1 By Pratik Das
+
+var year = 2100;
+
+if (year % 400 == 0) {
+  console.log("It is a Leap Year");
+} else if (year % 100 == 0) {
+  console.log("It is not a Leap Year");
+} else if (year % 4 == 0) {
+  printf("It is a Leap Year");
+} else {
+  console.log("It is not a Leap Year");
+}
