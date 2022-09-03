27a147e44e1b8306a95d2d02688cbb5bf730eb2a
Pratik Das <pratik14901@gmail.com>
2022-09-04 00:16:02 +0530

    asgn 4a

---
+++ JavaScript/Assignment 4/Question.js
@@ -0,0 +1,16 @@
+//Solution by Pratik Das
+
+/*
+    here the base case is for n = 1 where ans is 6. Now with in crease in n each common part will be ignored
+
+    / \/ \
+    |_|__|   => ans is 6*2 - (1 common part) = 13
+    
+    Thus in general ans will be (6*n) - (n-1)
+*/
+
+function matchHouses(n) {
+    return ((6*n) - (n-1));
+}
+
+console.log(matchHouses(87));
